178985df94356bf7b1691980131e5158c22b7f94
Gabriel Sabillon <gaboelnuevo@gmail.com>
2017-05-13 10:56:47 -0600

    create shedule

--- src/app/root/root.js
+++ src/app/root/root.js
@@ -1,3 +1,35 @@
+function diffMonth(startDate, endDate) {
+  var startMonths = startDate.getMonth() + (startDate.getFullYear() * 12);
+  var endMonths = endDate.getMonth() + (endDate.getFullYear() * 12);
+  return endMonths - startMonths;
+}
+
+function generateMonthly(startDate, endDate, times) {
+  var i;
+  var shedule = [];
+  for (i = 0; i < times; i++) {
+    var obj = {};
+    obj.startDate = i === 0 ? startDate : addDays(shedule[i - 1].endDate, 1);
+    obj.endDate = i === (times - 1) ? endDate : getLastDayOfMonth(obj.startDate);
+    shedule.push({
+      startDate: startDate,
+      endDate: endDate
+    });
+  }
+
+  return shedule;
+}
+
+function getLastDayOfMonth(date) {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0);
+}
+
+function addDays(date, days) {
+  var copyOf = new Date(date.valueOf());
+  var dateInMs = copyOf.setDate(copyOf.getDate() + days);
+  return new Date(dateInMs);
+}
+
 angular
   .module('app')
   .component('root', {
@@ -8,12 +40,11 @@ angular
       self.startDate = new Date();
       self.endDate = new Date();
       self.contryCode = 'us';
+      self.shedule = [];
       self.onGenerate = function () {
-        console.log({
-          startDate: self.startDate,
-          endDate: self.endDate,
-          contryCode: self.contryCode
-        });
+        var nMonths = diffMonth(self.startDate, self.endDate) + 1;
+        var shedule = generateMonthly(self.startDate, self.endDate, nMonths);
+        self.shedule = shedule;
       };
     }
   });
